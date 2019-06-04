673a5274175765621491c4b0e3a57cc51e777b96
gsaqui <glenn.saqui@gmail.com>
2019-06-04 11:54:43 -0700

    Adding routes and controllers

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Http\Request;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Foundation\Validation\ValidatesRequests;
@@ -10,4 +11,56 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    public function animals(Request $request, $type = null, $numberOfAnimals = 0)
+    {
+        $data = 'Hello World';
+        return response()->json($data);
+    }
+
+    private function allAnimals() : array
+    {
+        return [
+            [
+                "type" => 'dog',
+                'name' => 'bulldog'
+            ],
+            [
+                "type" => 'dog',
+                'name' => 'poodle'
+            ],
+            [
+                "type" => 'dog',
+                'name' => 'beagle'
+            ],
+            [
+                "type" => 'cat',
+                'name' => 'persian'
+            ],
+            [
+                "type" => 'cat',
+                'name' => 'russian blue'
+            ],
+            [
+                "type" => 'cat',
+                'name' => 'scottish fold'
+            ],
+            [
+                "type" => 'cat',
+                'name' => 'siamese'
+            ],
+            [
+                "type" => 'cow',
+                'name' => 'angus'
+            ],
+            [
+                "type" => 'cow',
+                'name' => 'hereford'
+            ],
+            [
+                "type" => 'cow',
+                'name' => 'guernsey'
+            ]
+        ];
+    }
 }
